d993f74bb42aae2b2e9fca33e637c94cd3c13de4
honglio <hong.liu@live.cn>
2014-05-31 21:08:12 +0800

    Add flash msg after email submit

--- js/jquery.contact.js
+++ js/jquery.contact.js
@@ -1,5 +1,5 @@
 
-(function($) { 
+(function($) {
 	"use strict";
 	jQuery(document).ready(function(){
 	$('#cform').submit(function(){
@@ -7,25 +7,23 @@
 		var action = $(this).attr('action');
 
 		$("#message").slideUp(750,function() {
-		$('#message').hide();
+			$('#message').hide();
 
- 		$('#submit')
-			.before('<img src="images/ajax-loader.gif" class="contact-loader" />')
-			.attr('disabled','disabled');
+	 		$('#submit')
+				.before('<img src="images/ajax-loader.gif" class="contact-loader" />')
+				.attr('disabled','disabled');
 
-		$.post(action, {
-			name: $('#name').val(),
-			email: $('#email').val(),
-			comments: $('#comments').val(),
-		},
-			function(data){
-				document.getElementById('message').innerHTML = data;
+			$.post(action, {
+				name: $('#name').val(),
+				email: $('#email').val(),
+				comments: $('#comments').val(),
+			}).fail(function(){
+				// document.getElementById('message').innerHTML = data;
 				$('#message').slideDown('slow');
 				$('#cform img.contact-loader').fadeOut('slow',function(){$(this).remove()});
 				$('#submit').removeAttr('disabled');
-				if(data.match('success') != null) $('#cform').slideUp('slow');
-			}
-		);
+				$('#cform').slideUp('slow');
+			});
 
 		});
 
@@ -34,5 +32,5 @@
 	});
 
 });
-	
+
 	}(jQuery));
