4891d63669d6eb36a6ae10e377714010005c98c7
honglio <hong.liu@live.cn>
2014-11-13 11:10:03 +0800

    Minor fixed

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -480,7 +480,7 @@ module.exports = function (grunt) {
 
     grunt.task.run([
       'clean:server',
-      'wiredep',
+      // 'wiredep',
       'concurrent:server',
       'autoprefixer',
       'connect:livereload',
